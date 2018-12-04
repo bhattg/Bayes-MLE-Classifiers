226193ca37fbd9309679c7a968cd61b2b749d61f
bhattg <iitdgantavya@gmail.com>
2018-12-04 16:04:24 +0530

    final changes

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from modifyInput import modifier
-from maths import mathematica
-import pandas as pd 
 from naiveBayes import naiveBayes
-from pathlib import Path 
-from bayes import bayes
-import numpy as np
+import pandas as pd
 
 link = "Medical_data.csv"
-testframe= pd.read_csv("T.csv")
+testframe= pd.read_csv("test_medical.csv")
 totalTestpoints=testframe.shape[0]
-
 classifier = naiveBayes(link)	
 classifier.fit()
-
 count=0
 for i in range(0, totalTestpoints):
 	
@@ -20,5 +14,5 @@ for i in range(0, totalTestpoints):
 	predicted=classifier.predict((testframe.iloc[i, 1:4]).tolist())
 	if predicted==original:
 		count=count+1
-
+	
 print("accuracy=", 100*count/totalTestpoints)
